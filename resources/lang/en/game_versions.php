29cc550dc129bb588b1af713ce4b8011186b4b4d
René Kooi <renee@kooi.me>
2016-10-19 12:04:52 +0200

    fix references to GameInfo::VERSION in language files

--- resources/lang/en/game_versions.php
+++ resources/lang/en/game_versions.php
@@ -1,21 +1,21 @@
 <?php
 
-use RecAnalyst\GameInfo;
+use RecAnalyst\Model\Version;
 
 return [
-    GameInfo::VERSION_UNKNOWN => 'Unknown',
-    GameInfo::VERSION_AOK => 'AOK',
-    GameInfo::VERSION_AOKTRIAL => 'AOK Trial',
-    GameInfo::VERSION_AOK20 => 'AOK 2.0',
-    GameInfo::VERSION_AOK20A => 'AOK 2.0a',
-    GameInfo::VERSION_AOC => 'AOC',
-    GameInfo::VERSION_AOCTRIAL => 'AOC Trial',
-    GameInfo::VERSION_AOC10 => 'AOC 1.0',
-    GameInfo::VERSION_AOC10C => 'AOC 1.0c',
-    GameInfo::VERSION_USERPATCH11 => 'AOC 1.1',
-    GameInfo::VERSION_USERPATCH12 => 'AOC UP1.2',
-    GameInfo::VERSION_USERPATCH13 => 'AOC UP1.3',
-    GameInfo::VERSION_USERPATCH14 => 'AOC UP1.4',
-    GameInfo::VERSION_AOFE21 => 'AOFE 2.1',
-    GameInfo::VERSION_HD => 'HD Edition',
+    Version::VERSION_UNKNOWN => 'Unknown',
+    Version::VERSION_AOK => 'AOK',
+    Version::VERSION_AOKTRIAL => 'AOK Trial',
+    Version::VERSION_AOK20 => 'AOK 2.0',
+    Version::VERSION_AOK20A => 'AOK 2.0a',
+    Version::VERSION_AOC => 'AOC',
+    Version::VERSION_AOCTRIAL => 'AOC Trial',
+    Version::VERSION_AOC10 => 'AOC 1.0',
+    Version::VERSION_AOC10C => 'AOC 1.0c',
+    Version::VERSION_USERPATCH11 => 'AOC 1.1',
+    Version::VERSION_USERPATCH12 => 'AOC UP1.2',
+    Version::VERSION_USERPATCH13 => 'AOC UP1.3',
+    Version::VERSION_USERPATCH14 => 'AOC UP1.4',
+    Version::VERSION_AOFE21 => 'AOFE 2.1',
+    Version::VERSION_HD => 'HD Edition',
 ];
