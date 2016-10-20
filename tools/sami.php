3fc9a6aa44811658008c2d3ef96c1d41e2d00ac2
René Kooi <renee@kooi.me>
2016-10-20 15:10:29 +0200

    sami: fix some paths

--- tools/sami.php
+++ tools/sami.php
@@ -8,12 +8,13 @@ use Sami\Sami;
 use Sami\Version\GitVersionCollection;
 use Sami\RemoteRepository\GitHubRemoteRepository;
 
-$dir = __DIR__ . '/../src/';
+$base = __DIR__ . '/..';
+$dir = $base . '/src';
 
 return new Sami($dir, [
     'title' => 'RecAnalyst API',
-    'build_dir' => __DIR__ . '/doc',
+    'build_dir' => $base . '/doc',
     'cache_dir' => __DIR__ . '/tmp',
-    'remote_repository' => new GitHubRemoteRepository('goto-bus-stop/recanalyst', '/src/'),
+    'remote_repository' => new GitHubRemoteRepository('goto-bus-stop/recanalyst', $base),
     'default_opened_level' => 2,
 ]);
