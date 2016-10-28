58bcb9efaea551ccdd9141815525709dc6606957
René Kooi <renee@kooi.me>
2016-10-28 22:58:18 +0200

    add test for drawing map images with lots of units

--- test/MapImageTest.php
+++ test/MapImageTest.php
@@ -7,12 +7,37 @@ use RecAnalyst\Processors\MapImage;
 
 class MapImageTest extends TestCase
 {
-    public function testImage()
+    public function setup()
     {
         MapImage::defaultManager(['driver' => 'imagick']);
+    }
+
+    public function testImage()
+    {
         $testPath = Path::join(__DIR__, uniqid('HD_test') . '.png');
 
         $rec = new RecordedGame(Path::join(__DIR__, './recs/versions/HD_test.mgx'));
+        $processor = new MapImage($rec);
+
+        $image = $processor->run();
+
+        // Not much else to do. I don't really know how to test this ¯\_(ツ)_/¯
+        $image->save($testPath);
+
+        $this->assertFileExists($testPath);
+
+        unlink($testPath);
+    }
+
+    /**
+     * Draw a map image for a campaign game, with lots and lots of units
+     * existing from the start.
+     */
+    public function testImageCampaign()
+    {
+        $testPath = Path::join(__DIR__, uniqid('SP Replay v4.6 @2015.12.29 001221') . '.png');
+
+        $rec = new RecordedGame(Path::join(__DIR__, './recs/versions/SP Replay v4.6 @2015.12.29 001221.aoe2record'));
         $processor = new MapImage($rec);
 
         $image = $processor->run();
