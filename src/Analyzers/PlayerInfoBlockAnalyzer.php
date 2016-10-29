476746b5186cb7aff065c7bc623749cf3eb5c3e5
René Kooi <renee@kooi.me>
2016-10-29 12:32:04 +0200

    move reading player object lists to separate analyzer

--- src/Analyzers/PlayerInfoBlockAnalyzer.php
+++ src/Analyzers/PlayerInfoBlockAnalyzer.php
@@ -72,15 +72,6 @@ class PlayerInfoBlockAnalyzer extends Analyzer
     private function analyzeExtended()
     {
         $existObjectSeparator = pack('c*', 0x0B, 0x00, 0x08, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00);
-        $objectEndSeparator =
-            pack('c*', 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x80, 0xBF, 0x00, 0x00, 0x80, 0xBF)
-            . pack('c*', 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF)
-            . pack('c*', 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
-        $aokObjectEndSeparator =
-            pack('c*', 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x80, 0xBF, 0x00)
-            . pack('c*', 0x00, 0x80, 0xBF, 0x00, 0x00, 0x00, 0x00, 0x00);
-        $playerInfoEndSeparator = pack('c*', 0x00, 0x0B, 0x00, 0x02, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x0B);
-        $objectsMidSeparatorGaia = pack('c*', 0x00, 0x0B, 0x00, 0x40, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00);
 
         $pack = $this->rec->getResourcePack();
 
@@ -153,13 +144,12 @@ class PlayerInfoBlockAnalyzer extends Analyzer
             } else {
                 $this->position += 5;
             }
-            $civilization = ord($this->header[$this->position]);
+            $civilization = ord($this->header[$this->position++]);
             if (!$civilization) {
                 $civilization = 1;
             }
-            $this->position += 1 + 3;
-            $playerColor = ord($this->header[$this->position]);
-            $this->position += 1;
+            $this->position += 3;
+            $playerColor = ord($this->header[$this->position++]);
 
             $player->civId = $civilization;
             $player->colorId = $playerColor;
@@ -176,7 +166,6 @@ class PlayerInfoBlockAnalyzer extends Analyzer
             $player->initialState->extraPop = $player->initialState->population -
                 ($player->initialState->civilianPop + $player->initialState->militaryPop);
 
-            // GAIA
             if ($version->isTrial) {
                 $this->position += 4;
             }
@@ -193,162 +182,17 @@ class PlayerInfoBlockAnalyzer extends Analyzer
                 $this->position = $existObjectPos + strlen($existObjectSeparator);
             }
 
-            $done = false;
-            while (!$done) {
-                $objectType = ord($this->header[$this->position]);
+            $objects = $this->read(PlayerObjectsListAnalyzer::class, [
+                'players' => array_merge([
+                    0 => $gaia
+                ], $playersByIndex),
+            ]);
 
-                $owner = null;
-                $ownerId = null;
-                if ($objectType !== 0) {
-                    $ownerId = ord($this->header[$this->position + 1]);
-                    $owner = $ownerId === 0 ? $gaia : $playersByIndex[$ownerId];
-                }
-
-                $this->position += 2;
-                $unitId = $this->readHeader('v', 2);
-
-                switch ($objectType) {
-                    case 10:
-                        if ($pack->isGaiaObject($unitId)) {
-                            $this->position += 19;
-                            $posX = $this->readHeader('f', 4);
-                            $posY = $this->readHeader('f', 4);
-                            $go = new Unit($this->rec, $unitId, [round($posX), round($posY)]);
-                            $this->gaiaObjects[] = $go;
-                            $this->position -= 27;
-                        }
-                        $this->position += 63 - 4;
-
-                        // TODO what's this?
-                        if ($version->isHDEdition) {
-                            $this->position += 3;
-                        }
-                        if ($version->isMgl) {
-                            $this->position += 1;
-                        }
-                        break;
-                    // Flags, Map Revealers, ???, only tends to appear in
-                    // scenario games.
-                    case 20:
-                        if ($version->isHDEdition) {
-                            $this->position += 3;
-                        }
-                        if ($version->isMgx) {
-                            $this->position += 59;
-                            $isExtended = ord($this->header[$this->position]);
-                            $this->position += 1; // $isExtended
-                            $this->position += 4;
-                            if ($isExtended == 2) {
-                                $this->position += 34;
-                            }
-                        } else {
-                            $this->position += 103 - 4;
-                        }
-                        break;
-                    case 30:
-                        // TODO what's this?
-                        if ($version->isHDEdition) {
-                            $this->position += 3;
-                        }
-                        if (!$version->isMgx) {
-                            $this->position += 1;
-                        }
-
-                        $isExtended = ord($this->header[$this->position + 59]);
-                        if ($isExtended === 2) {
-                            $this->position += 17;
-                        }
-
-                        $this->position += 204 - 4;
-
-                        if ($version->isHDPatch4) {
-                            $this->position += 1;
-                        }
-                        break;
-                    case 60:
-                        $b = ord($this->header[$this->position + 204]);
-                        $this->position += 233 - 4;
-
-                        if ($b) {
-                            $this->position += 67;
-                        }
-                        break;
-                    case 70:
-                        if ($pack->isGaiaUnit($unitId)) {
-                            $this->position += 19;
-                            $posX = $this->readHeader('f', 4);
-                            $posY = $this->readHeader('f', 4);
-                            $go = new Unit($this->rec, $unitId, [round($posX), round($posY)]);
-                            $this->gaiaObjects[] = $go;
-                        } else if ($owner) {
-                            // These units belong to someone!
-                            $this->position += 19;
-                            $posX = $this->readHeader('f', 4);
-                            $posY = $this->readHeader('f', 4);
-                            $uo = new Unit($this->rec, $unitId, [round($posX), round($posY)]);
-                            $uo->owner = $owner;
-                            $this->playerObjects[] = $uo;
-                        }
-                        if ($version->isMgx) {
-                            $separatorPos = strpos($this->header, $objectEndSeparator, $this->position);
-                            $this->position = $separatorPos + strlen($objectEndSeparator);
-                        } else {
-                            $separatorPos = strpos($this->header, $aokObjectEndSeparator, $this->position);
-                            $this->position = $separatorPos + strlen($aokObjectEndSeparator);
-                        }
-                        if ($separatorPos == -1) {
-                            throw new \Exception('Could not find object end separator');
-                        }
-                        break;
-                    case 80:
-                        if ($owner) {
-                            $this->position += 19;
-                            $posX = $this->readHeader('f', 4);
-                            $posY = $this->readHeader('f', 4);
-                            $uo = new Unit($this->rec, $unitId, [round($posX), round($posY)]);
-                            $uo->owner = $owner;
-                            $this->playerObjects[] = $uo;
-                        }
-
-                        if ($version->isMgx) {
-                            $separatorPos = strpos($this->header, $objectEndSeparator, $this->position);
-                            $this->position = $separatorPos + strlen($objectEndSeparator);
-                        } else {
-                            $separatorPos = strpos($this->header, $aokObjectEndSeparator, $this->position);
-                            $this->position = $separatorPos + strlen($aokObjectEndSeparator);
-                        }
-                        if ($separatorPos == -1) {
-                            throw new \Exception('Could not find object end separator');
-                        }
-
-                        $this->position += 126;
-                        if ($version->isMgx) {
-                            $this->position += 1;
-                        }
-
-                        if ($version->isHDPatch4) {
-                            $this->position -= 4;
-                        }
-                        break;
-                    case 00:
-                        $this->position -= 4;
-                        $buff = $this->readHeaderRaw(strlen($playerInfoEndSeparator));
-
-                        if ($buff === $playerInfoEndSeparator) {
-                            $done = true;
-                            break;
-                        }
-                        $this->position -= strlen($playerInfoEndSeparator);
-
-                        if ($buff[0] === $objectsMidSeparatorGaia[0] && $buff[1] === $objectsMidSeparatorGaia[1]) {
-                            $this->position += strlen($objectsMidSeparatorGaia);
-                        } else {
-                            throw new \Exception('Could not find GAIA object separator');
-                        }
-                        break;
-                    default:
-                        throw new \Exception(sprintf('Unknown object type %d', $objectType));
-                }
+            foreach ($objects->gaiaObjects as &$object) {
+                $this->gaiaObjects[] = $object;
+            }
+            foreach ($objects->playerObjects as &$object) {
+                $this->playerObjects[] = $object;
             }
         }
 
