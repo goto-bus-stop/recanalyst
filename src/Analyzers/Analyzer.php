d893a0fad5d631644aabe31f8fb97d3884b51624
Yvan Burrie <yvan.burrie@hotmail.com>
2017-02-18 19:49:13 +1000

    Update Analyzer.php
    Added a byte size information table for the pack() function. Also added an array reading method which is relatively fast.

--- src/Analyzers/Analyzer.php
+++ src/Analyzers/Analyzer.php
@@ -86,6 +86,39 @@ abstract class Analyzer
     }
 
     /**
+     * Information table for the number of bytes of each pack type.
+     *
+     * @var array
+     */
+    protected static $PACK_TYPE_BYTES = array(
+        'c' => 1,
+        's' => 2,
+        'l' => 4,
+        'f' => 4,
+        'd' => 8,
+    );
+
+    /**
+     * Read and unpack data from the header of the recorded game file with an array.
+     *
+     * @see https://secure.php.net/pack For documentation on data unpack format.
+     * @param string  $type  Data type.
+     * @param int $numElements
+     * @return \SplFixedArray
+     */
+    protected function readHeaderArray($type, $numElements)
+    {
+        $size = $numElements * $numBytes = self::$PACK_TYPE_BYTES[$type];
+        if ($this->position + $size > $this->headerSize) {
+            throw new \Exception('Can\'t read ' . $size . ' bytes');
+        }
+        // using the unpack function with a specified length is faster
+        $data = unpack($type.$numElements, substr($this->header, $this->position, $size));
+        $this->position += $size;
+        return \SplFixedArray::fromArray($data, false);
+    }
+
+    /**
      * Read raw strings from the header of the recorded game file.
      *
      * @param int  $size  Amount of characters to read.
