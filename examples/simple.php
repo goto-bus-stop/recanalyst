b6bb380602807c2299a71b25a94970adf1c267ff
Renée Kooi <renee@kooi.me>
2017-08-12 16:45:56 +0200

    Include `Version` string and number in examples/simple.php
    Makes it a bit more useful for debugging.

--- examples/simple.php
+++ examples/simple.php
@@ -9,21 +9,24 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use RecAnalyst\RecordedGame;
 
-$filename = __DIR__ . '/../test/recs/forgotten/HD-FE.mgx2';
+$filename = $argv[1] ?? __DIR__ . '/../test/recs/forgotten/HD-FE.mgx2';
 
 // Read a recorded game from a file path.
 $rec = new RecordedGame($filename);
 
-// Render a map image. Map images are instances of the \Intervention\Image
-// library, so you can easily manipulate them.
-$rec->mapImage()
-    ->resize(240, 120)
-    ->save('minimap.png');
+$version = $rec->version();
+echo 'Version: ' . $version->versionString . ' (' . $version->subVersion . ')' . "\n";
 
 // Display players and their civilizations.
 echo 'Players: ' . "\n";
 foreach ($rec->players() as $player) {
     echo ' * ' . $player->name . ' (' . $player->civName(). ')' . "\n";
 }
+
+// Render a map image. Map images are instances of the \Intervention\Image
+// library, so you can easily manipulate them.
+$rec->mapImage()
+    ->resize(240, 120)
+    ->save('minimap.png');
 
 echo 'Minimap saved in minimap.png.' . "\n";
