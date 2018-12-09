66845084858ec6ee6a520b2704f709e98767c4ef
Renée Kooi <renee@kooi.me>
2018-12-09 21:55:14 +0100

    idk

--- tools/make-doc.sh
+++ tools/make-doc.sh
@@ -1,8 +1,3 @@
-BRANCH="$(git branch | grep \* | cut -d ' ' -f2)"
-git pull origin master
-git checkout master
-git checkout $BRANCH
-
 curl -O https://get.sensiolabs.org/sami.phar
 php sami.phar update tools/sami.php
 
